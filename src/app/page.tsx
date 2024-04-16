88e60cd25a50ea6be99b8fe7ec9050682ea8728a
George Macwan <165771668+codal-georgem@users.noreply.github.com>
2024-04-16 20:29:16 +0530

    CIT-204 #time 1h create dashboard demo page regarding the demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,20 @@
-
 export default function Page() {
-  return <div>Welcome to Demo</div>;
+  return (
+    <>
+      <h1 className="mb-10 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        Next.js App Router Caching
+      </h1>
+      <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+        Welcome to our Next.js App Router Caching demo! Next.js is a React
+        framework that provides a robust environment for building server-side
+        rendered (SSR) and statically generated (SG) web applications. One of
+        its powerful features is the built-in routing system, which we'll be
+        exploring today.
+      </p>
+      <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+        In this demo, we'll cover various scenarios where router caching comes
+        into play, optimizing performance and enhancing user experience.
+      </p>
+    </>
+  );
 }
