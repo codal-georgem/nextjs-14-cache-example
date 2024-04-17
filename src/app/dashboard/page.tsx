9e35f6a633cb3fc4b776bdb94dba3d3232c15b7e
George Macwan <165771668+codal-georgem@users.noreply.github.com>
2024-04-17 14:33:21 +0530

    CIT-204 #time 1h css changes in the dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,30 +3,76 @@ import { Suspense } from "react";
 import Products from "../products/page";
 import { sql } from "@vercel/postgres";
 import { revalidateTag } from "next/cache";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default async function Dashboard() {
   const createProduct = async (formData: FormData) => {
-    "use server"; 
-    
+    "use server";
+
     const name = formData.get("name") as string;
     const price = formData.get("price") as string;
     const stock = 1;
     // console.log("values", name, price, stock);
     await sql`INSERT INTO Products (name, price, stock) VALUES (${name}, ${price}, ${stock});`;
+    
+    formData.delete("name");
+    formData.delete("price");
 
     revalidateTag("products");
   };
   return (
     <section>
       <h1 className="font-bold text-3xl">Dashboard</h1>
-      <Link href="/no-store">All Products</Link>
+      <Link
+        href="/no-store"
+        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+      >
+        All Products
+      </Link>
       <Suspense fallback={null}>
         <Products />
       </Suspense>
-      <form action={createProduct}>
-        <input name="name" />
-        <input name="price" type="number" />
-        <button type="submit">Create</button>
+      <form action={createProduct} className="space-y-8">
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
+          <div>
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Product Name
+            </label>
+            <Input
+              id="name"
+              name="name"
+              required
+              className="input input-bordered w-full max-w-xs"
+              placeholder="Enter name"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Price
+            </label>
+            <Input
+              id="price"
+              name="price"
+              type="number"
+              required
+              className="input input-bordered w-full max-w-xs"
+              placeholder="Enter value"
+            />
+          </div>
+        </div>
+        <Button
+          type="submit"
+          className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 me-2 mb-2 w-full"
+        >
+          Create
+        </Button>
       </form>
     </section>
   );
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,9 +4,14 @@ import { Suspense } from "react";
 async function ProductQunatity() {
   const data = await getProducts();
   return (
-    <ul className="pl-10">
+    <ul className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">
       {data?.data?.map((product) => (
-        <li key={product?.id}>{product?.name}</li>
+        <li
+          key={product?.id}
+          className="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400"
+        >
+          {product?.name}
+        </li>
       ))}
     </ul>
   );
@@ -15,7 +20,9 @@ async function ProductQunatity() {
 export default async function Products() {
   return (
     <section>
-      <h1>Products</h1>
+      <h1 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">
+        Products
+      </h1>
       <Suspense fallback={null}>
         <ProductQunatity />
       </Suspense>
